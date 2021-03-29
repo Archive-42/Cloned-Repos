2026e145457dd0f8ad89878851c6d512b6cff07b
Bryan C Guner <bryan.guner@gmail.com>
2021-03-29 06:37:19 +0000

    content

---
+++ _CONTAINER/__SANDBOX/functions-samples-master/message-translation/functions/index.js
@@ -0,0 +1,59 @@
+/**
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+const request = require('request-promise');
+
+// List of output languages.
+const LANGUAGES = ['en', 'es', 'de', 'fr', 'sv', 'ga', 'it', 'jp'];
+
+// Translate an incoming message.
+exports.translate = functions.database.ref('/messages/{languageID}/{messageID}').onWrite(event => {
+  const snapshot = event.data;
+  if (snapshot.val().translated) {
+    return;
+  }
+  const promises = [];
+  for (let i = 0; i < LANGUAGES.length; i++) {
+    var language = LANGUAGES[i];
+    if (language !== event.params.languageID) {
+      promises.push(createTranslationPromise(event.params.languageID, language, snapshot));
+    }
+  }
+  return Promise.all(promises);
+});
+
+// URL to the Google Translate API.
+function createTranslateUrl(source, target, payload) {
+  return `https://www.googleapis.com/language/translate/v2?key=${functions.config().firebase.apiKey}&source=${source}&target=${target}&q=${payload}`;
+}
+
+function createTranslationPromise(source, target, snapshot) {
+  const key = snapshot.key;
+  const message = snapshot.val().message;
+  return request(createTranslateUrl(source, target, message), {resolveWithFullResponse: true}).then(
+      response => {
+        if (response.statusCode === 200) {
+          const data = JSON.parse(response.body).data;
+          return admin.database().ref(`/messages/${target}/${key}`)
+              .set({message: data.translations[0].translatedText, translated: true});
+        }
+        throw response.body;
+      });
+}
---
+++ _CONTAINER/__SANDBOX/functions-samples-master/moderate-images/functions/index.js
@@ -0,0 +1,80 @@
+/**
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for t`he specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+const functions = require('firebase-functions');
+const mkdirp = require('mkdirp-promise');
+const gcs = require('@google-cloud/storage')();
+const vision = require('@google-cloud/vision')();
+const spawn = require('child-process-promise').spawn;
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
+
+/**
+ * When an image is uploaded we check if it is flagged as Adult or Violence by the Cloud Vision
+ * API and if it is we blur it using ImageMagick.
+ */
+exports.blurOffensiveImages = functions.storage.object().onChange(event => {
+  const object = event.data;
+  const file = gcs.bucket(object.bucket).file(object.name);
+
+  // Exit if this is a move or deletion event.
+  if (object.resourceState === 'not_exists') {
+    return console.log('This is a deletion event.');
+  }
+
+  // Check the image content using the Cloud Vision API.
+  return vision.detectSafeSearch(file).then(data => {
+    const safeSearch = data[0];
+    console.log('SafeSearch results on image', safeSearch);
+
+    if (safeSearch.adult || safeSearch.violence) {
+      return blurImage(object.name, object.bucket, object.metadata);
+    }
+  });
+});
+
+/**
+ * Blurs the given image located in the given bucket using ImageMagick.
+ */
+function blurImage(filePath, bucketName, metadata) {
+  const tempLocalFile = path.join(os.tmpdir(), filePath);
+  const tempLocalDir = path.dirname(tempLocalFile);
+  const bucket = gcs.bucket(bucketName);
+
+  // Create the temp directory where the storage file will be downloaded.
+  return mkdirp(tempLocalDir).then(() => {
+    console.log('Temporary directory has been created', tempLocalDir);
+    // Download file from bucket.
+    return bucket.file(filePath).download({destination: tempLocalFile});
+  }).then(() => {
+    console.log('The file has been downloaded to', tempLocalFile);
+    // Blur the image using ImageMagick.
+    return spawn('convert', [tempLocalFile, '-channel', 'RGBA', '-blur', '0x8', tempLocalFile]);
+  }).then(() => {
+    console.log('Blurred image created at', tempLocalFile);
+    // Uploading the Blurred image.
+    return bucket.upload(tempLocalFile, {
+      destination: filePath,
+      metadata: {metadata: metadata} // Keeping custom metadata.
+    });
+  }).then(() => {
+    console.log('Blurred image uploaded to Storage at', filePath);
+    fs.unlinkSync(tempLocalFile);
+    console.log('Deleted local file', filePath);
+  });
+}
